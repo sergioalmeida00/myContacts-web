da0c7bba145adcd7afe0046ada6bf0a01582c808
sergio almeida <sergioalmeidaa00@gmail.com>
2023-04-25 22:12:22 -0300

    FEAT:

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -44,3 +44,11 @@ export function Home(){
       </Container>
     )
 }
+
+fetch('http://localhost:3001/categories')
+  .then((response) => {
+    console.log('response', response)
+  })
+  .catch((error) => {
+    console.log('erro',error )
+  })
